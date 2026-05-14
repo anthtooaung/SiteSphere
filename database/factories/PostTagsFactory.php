859418545c81ebbd04d62be3f7408d9cddd1c4ab
anthtooaung <anthtooaung2792005@gmail.com>
2026-05-14 10:23:52 +0630

    create_post_tags

---
+++ database/factories/PostTagsFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\PostTags;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<PostTags>
+ */
+class PostTagsFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}

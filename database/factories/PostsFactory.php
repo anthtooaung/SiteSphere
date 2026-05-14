0eff92a34d828caa552bf31eff3a54583dc2b315
anthtooaung <anthtooaung2792005@gmail.com>
2026-05-14 09:08:45 +0630

    created the post table

---
+++ database/factories/PostsFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Posts;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Posts>
+ */
+class PostsFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}

101ec3fd384e60d066869f1b6a8dd52e5d44cdd0
anthtooaung <anthtooaung2792005@gmail.com>
2026-05-15 09:53:33 +0630

    notification

---
+++ app/Http/Controllers/NotificatioinsController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreNotificatioinsRequest;
+use App\Http\Requests\UpdateNotificatioinsRequest;
+use App\Models\Notificatioins;
+
+class NotificatioinsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreNotificatioinsRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Notificatioins $notificatioins)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Notificatioins $notificatioins)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateNotificatioinsRequest $request, Notificatioins $notificatioins)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Notificatioins $notificatioins)
+    {
+        //
+    }
+}

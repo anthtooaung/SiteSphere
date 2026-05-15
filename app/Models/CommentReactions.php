79f9c247e440c74a1befda137b8c332c44726760
anthtooaung <anthtooaung2792005@gmail.com>
2026-05-15 09:49:30 +0630

    comment_reaction

---
+++ app/Models/CommentReactions.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CommentReactions extends Model
+{
+    /** @use HasFactory<\Database\Factories\CommentReactionsFactory> */
+    use HasFactory;
+}

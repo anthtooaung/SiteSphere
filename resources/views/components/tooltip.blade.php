e78b02c98bd1cebe5f6af26f34a31b02e9bfd48b
anthtooaung <anthtooaung2792005@gmail.com>
2026-06-17 09:17:32 +0630

    fix: add ui-tooltip styles and aria-describedby for tooltip accessibility

--- resources/views/components/tooltip.blade.php
+++ resources/views/components/tooltip.blade.php
@@ -1,6 +1,6 @@
 @props(['content'])
 
-<div x-data="{ show: false }" {{ $attributes->merge(['class' => 'relative inline-flex']) }} @mouseenter="show = true" @mouseleave="show = false" @focusin="show = true" @focusout="show = false">
+<div x-data="{ show: false, tooltipId: 'tooltip-' + Math.random().toString(36).substr(2, 9) }" {{ $attributes->merge(['class' => 'relative inline-flex']) }} @mouseenter="show = true" @mouseleave="show = false" @focusin="show = true" @focusout="show = false">
     <div x-show="show" 
          x-transition:enter="transition ease-out duration-200"
          x-transition:enter-start="opacity-0 translate-y-1"
@@ -8,10 +8,13 @@
          x-transition:leave="transition ease-in duration-150"
          x-transition:leave-start="opacity-100 translate-y-0"
          x-transition:leave-end="opacity-0 translate-y-1"
+         :id="tooltipId"
          role="tooltip"
          x-cloak
          class="absolute z-50 px-2 py-1 text-xs rounded shadow-lg whitespace-nowrap -top-8 left-1/2 -translate-x-1/2 pointer-events-none ui-tooltip">
         {{ $content }}
     </div>
-    {{ $slot }}
+    <div x-bind:aria-describedby="tooltipId" class="inline-flex">
+        {{ $slot }}
+    </div>
 </div>

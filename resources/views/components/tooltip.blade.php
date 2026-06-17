238654b83fc62d61c41e4906b56435eb2006ce92
anthtooaung <anthtooaung2792005@gmail.com>
2026-06-17 09:07:51 +0630

    feat(tooltip): make tooltip theme-aware and support attribute merging

--- resources/views/components/tooltip.blade.php
+++ resources/views/components/tooltip.blade.php
@@ -1,6 +1,6 @@
 @props(['content'])
 
-<div x-data="{ show: false }" class="relative inline-flex" @mouseenter="show = true" @mouseleave="show = false" @focusin="show = true" @focusout="show = false">
+<div x-data="{ show: false }" {{ $attributes->merge(['class' => 'relative inline-flex']) }} @mouseenter="show = true" @mouseleave="show = false" @focusin="show = true" @focusout="show = false">
     <div x-show="show" 
          x-transition:enter="transition ease-out duration-200"
          x-transition:enter-start="opacity-0 translate-y-1"
@@ -10,7 +10,7 @@
          x-transition:leave-end="opacity-0 translate-y-1"
          role="tooltip"
          x-cloak
-         class="absolute z-50 px-2 py-1 text-xs text-white bg-gray-800 rounded shadow-lg whitespace-nowrap -top-8 left-1/2 -translate-x-1/2 pointer-events-none">
+         class="absolute z-50 px-2 py-1 text-xs rounded shadow-lg whitespace-nowrap -top-8 left-1/2 -translate-x-1/2 pointer-events-none ui-tooltip">
         {{ $content }}
     </div>
     {{ $slot }}
